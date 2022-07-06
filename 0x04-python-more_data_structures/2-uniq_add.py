ec24f7bc3239e8e825b25bce6a06bdb49c7abf1f
Abresh273 <101420907+Abresh273@users.noreply.github.com>
2022-07-06 15:51:17 +0300

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
    x=[]
+   res=0
    for i in my list:
        if i not in x:
            x.append(i)
-   return x
+   for i in x:
+      res+=i
+   return res
